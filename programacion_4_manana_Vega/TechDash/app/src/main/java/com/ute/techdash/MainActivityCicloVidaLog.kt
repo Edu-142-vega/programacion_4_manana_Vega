a966c635533baee3ea637f4cb8a0e0de7e9a79f1
Edu-142-vega <vega.eduardo5051@gmail.com>
2026-06-10 05:56:47 -0500

    CHECK 10-06-2026-01

--- programacion_4_manana_Vega/TechDash/app/src/main/java/com/ute/techdash/MainActivityCicloVidaLog.kt
+++ programacion_4_manana_Vega/TechDash/app/src/main/java/com/ute/techdash/MainActivityCicloVidaLog.kt
@@ -13,7 +13,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.ute.techdash.ui.theme.TechDashTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivityCicloVidaLog : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
